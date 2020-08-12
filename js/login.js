7f683cd504fb223eadc2cb4ec40569876a6414af
letidonatti <letidonatti413@gmail.com>
2020-08-12 19:44:54 -0300

    con mensajes de validaciones y localstorage

--- js/login.js
+++ js/login.js
@@ -1,16 +1,24 @@
 
-var errorSpan = document.getElementById("formErrorUser");
-var input = document.getElementById("username");
 
-input.addEventListener('invalid', function (e) {
-    errorSpan.innerHTML = "El nombre de usuario debe tener entre 1 y 15 caracteres";
-});
 
-var errorSpan = document.getElementById("formErrorPass");
-var input = document.getElementById("inputPassword");
+var inputUser = document.getElementById("username");
+var inputPass = document.getElementById("inputPassword");
+ 
+inputUser.addEventListener('invalid', function (e) {
+    e.preventDefault();
+    var errorSpan = document.getElementById("formErrorUser");
+    errorSpan.innerHTML = "El nombre de usuario no puede ser vacío";
 
-input.addEventListener('invalid', function (e) {
-    errorSpan.innerHTML = "La contraseña debe tener entre 6 y 8 caracteres";
+    inputPass.addEventListener('invalid', function (e) {
+        e.preventDefault();
+        var errorSpan = document.getElementById("formErrorPass");
+        errorSpan.innerHTML = "La contraseña debe tener entre 6 y 10 caracteres";
+    });
+}); 
+inputPass.addEventListener('invalid', function (e) {
+    e.preventDefault();
+    var errorSpan = document.getElementById("formErrorPass");
+    errorSpan.innerHTML = "Ingrese su contraseña";
 });
 
     
